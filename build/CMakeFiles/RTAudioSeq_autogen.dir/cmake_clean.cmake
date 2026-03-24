7b6a28ec15c51dcb851adf09bc7f90d6e2ea12cb
rtaudiolinuxv1-gt <rtaudiolinux.v1@gmail.com>
2026-03-24 00:36:09 +0000

    Add all my project files

--- build/CMakeFiles/RTAudioSeq_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/RTAudioSeq_autogen"
-  "RTAudioSeq_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/RTAudioSeq_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
